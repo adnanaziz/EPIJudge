282bf78bfefc2040749f66dd8ba084c5ebc78e33
Viacheslav Kroilov <slavakroilov@gmail.com>
2018-01-31 11:21:00 +0100

    Revert "Fix cpp param names"
     to prevent conflicts on https://github.com/adnanaziz/EPIJudge/pull/21

--- epi_judge_cpp/buy_and_sell_stock_k_times.cc
+++ epi_judge_cpp/buy_and_sell_stock_k_times.cc
@@ -10,7 +10,7 @@ double BuyAndSellStockKTimes(const vector<double>& prices, int k) {
 #include "test_framework/test_utils_generic_main.h"
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> param_names{"prices", "k"};
+  std::vector<std::string> param_names{"&prices", "k"};
   generic_test_main(argc, argv, param_names, "buy_and_sell_stock_k_times.tsv",
                     &BuyAndSellStockKTimes);
   return 0;
