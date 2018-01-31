5941789667ef09c32cb43af74f3534f861d146c3
Viacheslav Kroilov <slavakroilov@gmail.com>
2018-01-31 11:22:00 +0100

    Fix cpp parameter names

--- epi_judge_cpp/regular_expression.cc
+++ epi_judge_cpp/regular_expression.cc
@@ -10,7 +10,7 @@ bool IsMatch(const string &regex, const string &s) {
 #include "test_framework/test_utils_generic_main.h"
 
 int main(int argc, char *argv[]) {
-  std::vector<std::string> param_names{"&regex", "&s"};
+  std::vector<std::string> param_names{"regex", "s"};
   generic_test_main(argc, argv, param_names, "regular_expression.tsv",
                     &IsMatch);
   return 0;
