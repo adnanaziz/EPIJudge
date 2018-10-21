5c7489e8212fe4d2f9388a369b8c1cd98bf603e0
Firat Beytemür <ga25gux@mytum.de>
2018-10-22 00:10:35 +0200

    test(dataset): added test dataset

--- test/heatmap_test.py
+++ test/heatmap_test.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('../')
 import feature_selection.heatmaps as H
 
-path_to_features1 = "/nas/lrz/tuei/ldv/studierende/Emotion/IEMOCAP_full_release/same_length/nsynth_features/"
+path_to_features6 = "/nas/lrz/tuei/ldv/studierende/Emotion/IEMOCAP_full_release/same_length/nsynth_features/"
 
 path_to_features2 = '/nas/lrz/tuei/ldv/studierende/Emotion/IEMOCAP_full_release/same_length/pyaudio_more_cat/'
 
@@ -15,6 +15,8 @@ path_to_labels = '/nas/lrz/tuei/ldv/studierende/' + \
 
 path_to_features5 = "/nas/lrz/tuei/ldv/studierende/Emotion/IEMOCAP_full_release/same_length/opensmile/"
 
+path_to_features1 = "/home/ga25gux/tensorflow_python2/EPIJudge/example_dataset/"
+
 path_list = [path_to_features1, path_to_features2, path_to_features3,
              path_to_features4, path_to_features5]
 
@@ -22,6 +24,6 @@ folder = "plot/"
 
 name = ['nsynth', 'pyAudio',
         'mel_band', 'pyAudio', 'opensmile']
-for i in xrange(5):
+for i in xrange(1):
     heat = H.Heatmap(path_list[i], path_to_labels, name[i])
     heat.create_heat_map(folder)
