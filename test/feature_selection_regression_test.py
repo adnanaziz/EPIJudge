abcb9e88163ce6cdb77355cc3ad01ec5b8cf0441
Firat Beytemür <ga25gux@mytum.de>
2018-10-22 00:31:53 +0200

    test(dataset): added example dataset for tests

--- test/feature_selection_regression_test.py
+++ test/feature_selection_regression_test.py
@@ -9,24 +9,14 @@ import feature_selection.featureselection as feature
 
 path_to_features1 = "/nas/lrz/tuei/ldv/studierende/Emotion/IEMOCAP_full_release/same_length/nsynth_features/"
 
-path_to_features2 = '/nas/lrz/tuei/ldv/studierende/Emotion/IEMOCAP_full_release/same_length/pyaudio_more_cat/'
-
-path_to_features3 = "/nas/lrz/tuei/ldv/studierende/Emotion/IEMOCAP_full_release/same_length/mel/"
-
-path_to_features4 = '/nas/lrz/tuei/ldv/studierende/Emotion/IEMOCAP_full_release/same_length/pyaudio_less_cat/'
-
-path_to_features5 = "/nas/lrz/tuei/ldv/studierende/Emotion/IEMOCAP_full_release/same_length/opensmile/"
-
 std = False
 MI = False
 
 plot = "plot/"
 
-features = ['nsynth', 'pyaudio_more_cat',
-            'mel_frequency_energy', 'pyaudio_less_cat', 'opensmile_IS_09']
+features = 'nsynth'
 
-path_list = [path_to_features1, path_to_features2, path_to_features3,
-             path_to_features4, path_to_features5]
+path_list = path_to_features1
 
 path_to_labels = '/nas/lrz/tuei/ldv/studierende/' + \
                  'Emotion/IEMOCAP_full_release/IEMOCAP_labels_regression/'
@@ -35,27 +25,27 @@ component_list = [16, 17, 18, 15, 15]
 
 np.random.seed(200)
 
-for l in xrange(1):
-    path_to_features = path_list[l]
-    print features[l]
-    if features[l] == 'pyaudio_less_cat':
-        emotions = ['ang', 'exc', 'neu', 'sad']
-    else:
-        emotions = ['ang', 'exc', 'neu', 'sad', 'fru', 'hap', 'sur',
-                    'fea', 'xxx']
-    data = feature.Load_features(path_to_features, path_to_labels, features[l],
-                                 emotions, reg=True)
-    x, y = data.data()
-    if std is True:
-        plot = plot + 'std/'
-    if MI is True:
-        plot = plot + '_MI_'
 
-    plsda = feature.PLSDA(x, y, features[l], plot, reg=True)
-    plsda.PLS_process(plot)
-    selection = feature.feature_selection(x, y, features[l], plot, reg=True)
-    selection.mutual_information()
-    selection.plot_features()
+path_to_features = path_list
+print features
+if features == 'pyaudio_less_cat':
+    emotions = ['ang', 'exc', 'neu', 'sad']
+else:
+    emotions = ['ang', 'exc', 'neu', 'sad', 'fru', 'hap', 'sur',
+                'fea', 'xxx']
+data = feature.Load_features(path_to_features, path_to_labels, features,
+                             emotions, reg=True)
+x, y = data.data()
+if std is True:
+    plot = plot + 'std/'
+if MI is True:
+    plot = plot + '_MI_'
 
-    test = feature.final_test()
-    test.plot(x, y, features[l], plot, component_list[l], reg=True)
+plsda = feature.PLSDA(x, y, features, plot, reg=True)
+plsda.PLS_process(plot)
+selection = feature.feature_selection(x, y, features, plot, reg=True)
+selection.mutual_information()
+selection.plot_features()
+
+test = feature.final_test()
+test.plot(x, y, features, plot, component_list, reg=True)
--- test/heatmap_test.py
+++ test/heatmap_test.py
@@ -2,28 +2,16 @@ import sys
 sys.path.append('../')
 import feature_selection.heatmaps as H
 
-path_to_features6 = "/nas/lrz/tuei/ldv/studierende/Emotion/IEMOCAP_full_release/same_length/nsynth_features/"
-
-path_to_features2 = '/nas/lrz/tuei/ldv/studierende/Emotion/IEMOCAP_full_release/same_length/pyaudio_more_cat/'
-
-path_to_features3 = '/nas/lrz/tuei/ldv/studierende/Emotion/IEMOCAP_full_release/same_length/mel/'
-
-path_to_features4 = '/nas/lrz/tuei/ldv/studierende/Emotion/IEMOCAP_full_release/same_length/pyaudio_less_cat/'
-
 path_to_labels = '/nas/lrz/tuei/ldv/studierende/' + \
                  'Emotion/IEMOCAP_full_release/IEMOCAP_labels_regression/'
 
-path_to_features5 = "/nas/lrz/tuei/ldv/studierende/Emotion/IEMOCAP_full_release/same_length/opensmile/"
-
 path_to_features1 = "/home/ga25gux/tensorflow_python2/EPIJudge/example_dataset/"
 
-path_list = [path_to_features1, path_to_features2, path_to_features3,
-             path_to_features4, path_to_features5]
 
 folder = "plot/"
 
-name = ['nsynth', 'pyAudio',
-        'mel_band', 'pyAudio', 'opensmile']
-for i in xrange(1):
-    heat = H.Heatmap(path_list[i], path_to_labels, name[i])
-    heat.create_heat_map(folder)
+name = 'nsynth'
+
+
+heat = H.Heatmap(path_to_features1, path_to_labels, name)
+heat.create_heat_map(folder)
