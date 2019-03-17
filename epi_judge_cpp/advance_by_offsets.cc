b193b96fc98b65364f694a504f607749dcca7ed8
siddkuma <siddkuma@adobe.com>
2019-03-17 18:23:45 +0530

    6.4, 6.5
    6.4, 6.5

--- epi_judge_cpp/advance_by_offsets.cc
+++ epi_judge_cpp/advance_by_offsets.cc
@@ -1,9 +1,42 @@
 #include <vector>
 #include "test_framework/generic_test.h"
 using std::vector;
-bool CanReachEnd(const vector<int>& max_advance_steps) {
-  // TODO - you fill in here.
-  return true;
+// bool CanReachEnd(const vector<int>& A) {
+//     int n = A.size();
+//     if (n == 1) return true;
+
+//     vector<bool> reach(n, false);
+//     reach[n-1] = true;
+
+//     for (int i=n-2; i>=0; i--)
+//     {
+//         for (int j=i; j<i+A[i]+1; j++)
+//         {
+//             if (reach[j] == true)
+//             {
+//                 reach[i] = true;
+//                 break;
+//             }
+//         }
+//     }
+
+//     return reach[0];
+// }
+
+bool CanReachEnd(const vector<int>& A) {
+    int n = A.size();
+    if (n == 1) return true;
+    vector<int> reach(n, 0);
+    int max_till_now = 0;
+
+    for (int i=0; i<n; i++)
+    {
+        if (i > max_till_now) return false;
+        reach[i] = std::max(A[i]+i, max_till_now);
+        if (reach[i] > max_till_now) max_till_now = reach[i];
+        if (max_till_now == n-1) return true;
+    }
+    return true;
 }
 
 int main(int argc, char* argv[]) {
