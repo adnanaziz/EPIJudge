d863f7363b01bcacb54d6a6a5a3642be906f57b7
ksidiqi <ksidiqi@users.noreply.github.com>
2019-03-24 23:25:56 -0700

    Update search_first_key.py
    cleaner way

--- epi_judge_python_solutions/search_first_key.py
+++ epi_judge_python_solutions/search_first_key.py
@@ -4,19 +4,14 @@ from test_framework import generic_test
 
 
 def search_first_of_k(A, k):
-
-    left, right, result = 0, len(A) - 1, -1
-    # A[left:right + 1] is the candidate set.
-    while left <= right:
-        mid = (left + right) // 2
-        if A[mid] > k:
-            right = mid - 1
-        elif A[mid] == k:
-            result = mid
-            right = mid - 1  # Nothing to the right of mid can be solution.
-        else:  # A[mid] < k.
-            left = mid + 1
-    return result
+    lo, hi = 0, len(A)
+    while lo < hi:
+        mid = lo + (hi - lo) // 2
+        if k <= A[mid]:
+            hi = mid
+        else:
+            lo = mid + 1
+    return lo if A[lo] == k else -1
 
 
 # Pythonic solution
