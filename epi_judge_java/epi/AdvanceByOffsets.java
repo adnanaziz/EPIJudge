6e5a319e719d36e7dc1bf15bdaf19b5712e61792
Viacheslav Kroilov <slavakroilov@gmail.com>
2019-11-30 11:54:49 +0100

    Revert "Add dummy change"
    This reverts commit 4e368d91d9f425c0745dc120378f4e8b92d88068.

--- epi_judge_java/epi/AdvanceByOffsets.java
+++ epi_judge_java/epi/AdvanceByOffsets.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class AdvanceByOffsets {
   @EpiTest(testDataFile = "advance_by_offsets.tsv")
   public static boolean canReachEnd(List<Integer> maxAdvanceSteps) {
-    // Change
     // TODO - you fill in here.
     return true;
   }
