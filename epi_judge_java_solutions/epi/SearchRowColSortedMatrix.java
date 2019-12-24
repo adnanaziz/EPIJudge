469f706b3406226575907d5eb9d84dbeabba63e6
Abdul Qadeer <quadeer.leo@gmail.com>
2019-12-23 21:14:37 -0800

    Provide O(log(mn)) solution

--- epi_judge_java_solutions/epi/SearchRowColSortedMatrix.java
+++ epi_judge_java_solutions/epi/SearchRowColSortedMatrix.java
@@ -6,22 +6,38 @@ import epi.test_framework.GenericTest;
 import java.util.List;
 
 public class SearchRowColSortedMatrix {
+  
   @EpiTest(testDataFile = "search_row_col_sorted_matrix.tsv")
-
   public static boolean matrixSearch(List<List<Integer>> A, int x) {
-
-    int row = 0, col = A.get(0).size() - 1; // Start from the top-right corner.
-    // Keeps searching while there are unclassified rows and columns.
-    while (row < A.size() && col >= 0) {
-      if (A.get(row).get(col).equals(x)) {
+    int rowSize = A.get(0).size();
+    int colSize = A.size();
+    for (int i = 0; i < Math.min(rowSize, colSize); i++) {
+      if (matrixBinarySearch(A, i, false, x) || matrixBinarySearch(A, i, true, x)) {
         return true;
-      } else if (A.get(row).get(col) < x) {
-        ++row; // Eliminate this row.
-      } else { // A.get(row).get(col) > x.
-        --col; // Eliminate this column.
       }
     }
     return false;
+  }
+
+  private static boolean matrixBinarySearch(List<List<Integer>> A, int index, boolean colSearch, int target) {
+    int lo = index, hi = colSearch ? A.size() - 1: A.get(0).size() - 1;
+    while (lo < hi) {
+      int mid = lo + (hi-lo)/2;
+      if (colSearch) {
+        if (A.get(mid).get(index) >= target) {
+          hi = mid;
+        } else {
+          lo = mid + 1;
+        }
+      } else {
+        if (A.get(index).get(mid) >= target) {
+          hi = mid;
+        } else {
+          lo = mid + 1;
+        }
+      }
+    }
+    return colSearch && A.get(lo).get(index) == target || !colSearch && A.get(index).get(lo) == target;
   }
 
   public static void main(String[] args) {
