a849b65741cac5a56414c6a9559eef95f0a1183b
Chris Terrazas <cterrazas69@gmail.com>
2020-02-04 14:21:47 -0500

    Increased performance of int_as_array_multiply solution.

--- epi_judge_python_solutions/int_as_array_multiply.py
+++ epi_judge_python_solutions/int_as_array_multiply.py
@@ -4,22 +4,22 @@ from test_framework import generic_test
 
 
 def multiply(num1: List[int], num2: List[int]) -> List[int]:
-
-    sign = -1 if (num1[0] < 0) ^ (num2[0] < 0) else 1
-    num1[0], num2[0] = abs(num1[0]), abs(num2[0])
-
-    result = [0] * (len(num1) + len(num2))
-    for i in reversed(range(len(num1))):
-        for j in reversed(range(len(num2))):
-            result[i + j + 1] += num1[i] * num2[j]
-            result[i + j] += result[i + j + 1] // 10
-            result[i + j + 1] %= 10
-
-    # Remove the leading zeroes.
-    result = result[next((i for i, x in enumerate(result)
-                          if x != 0), len(result)):] or [0]
-    return [sign * result[0]] + result[1:]
-
+    num1str, num2str = "", ""
+    result = []
+    for n in num1: num1str += str(n)
+    for n in num2: num2str += str(n)
+    resultnum = str(int(num1str) * int(num2str))
+    if resultnum[0] == "-":
+        negNum = resultnum[1:]
+        for ind, n in enumerate(negNum):
+            if ind == 0:
+                result.append(-int(n))
+            else:
+                result.append(int(n))
+    else:
+        for n in resultnum:
+            result.append(int(n))
+    return result
 
 if __name__ == '__main__':
     exit(
