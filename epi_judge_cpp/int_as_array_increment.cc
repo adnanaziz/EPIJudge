a9ac0be8eee2639d34902fa59aec41513fa3dbd7
siddkuma <siddkuma@adobe.com>
2020-05-14 07:22:02 +0530

    Revert "5.1, 6.2"
    This reverts commit 8681719579b920dfd8d3890a3783402e8692d6a7.

--- epi_judge_cpp/int_as_array_increment.cc
+++ epi_judge_cpp/int_as_array_increment.cc
@@ -2,17 +2,8 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 vector<int> PlusOne(vector<int> A) {
-    int i = A.size()-1;
-    int carry = 1;
-    while (carry != 0 && i >= 0)
-    {
-        int new_val = A[i]+carry;
-        A[i] = new_val%10;
-        carry = new_val/10;
-        i--;
-    }
-    if (carry != 0) A.insert(A.begin(), 1);
-    return A;
+  // TODO - you fill in here.
+  return {};
 }
 
 int main(int argc, char* argv[]) {
--- epi_judge_cpp/parity.cc
+++ epi_judge_cpp/parity.cc
@@ -1,25 +1,7 @@
 #include "test_framework/generic_test.h"
-
-// short Parity(unsigned long long x) {
-//     // TODO - you fill in here.
-//     short ans = 0;
-//     while (x)
-//     {
-//         ans ^= x&1;
-//         x >>= 1;
-//     }
-//     return ans;
-// }
-
 short Parity(unsigned long long x) {
-    // TODO - you fill in here.
-    short ans = 0;
-    while (x)
-    {
-        ans ^= 1;
-        x &= (x-1); // replace the lowest set-bit to 0
-    }
-    return ans;
+  // TODO - you fill in here.
+  return 0;
 }
 
 int main(int argc, char* argv[]) {
