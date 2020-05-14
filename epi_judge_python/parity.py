f5210913061b7bfd02d4d8feb18c5b39dedd1d28
John Mwaniki <johnmejohn1@gmail.com>
2020-05-13 23:42:37 -0400

    practice/primitives: add improved version of parity from O(n) to O(k)
    Instead of going through every bit of the input, we only iterate over
    the set bits. This is achieved by droppong off the lowest set bit of the
    input after each loop.
    
    The operation x & (x-1) drops off the lowest set bit of x.
    
    To flip the bit, the ^ (XOR) operation is used

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -4,8 +4,8 @@ from test_framework import generic_test
 def parity(x: int) -> int:
     result = 0 
     while x:
-        result ^= x & 1
-        x = x >> 1
+      result ^= 1 
+      x &= (x-1)
     return result 
 
 
