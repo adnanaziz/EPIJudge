437394c7551c2f469089031ba8c4c58cadeeabfe
John Mwaniki <johnmejohn1@gmail.com>
2020-05-13 23:33:51 -0400

    practice/primitives: add bruteforce solution of parity

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,8 +2,11 @@ from test_framework import generic_test
 
 
 def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    result = 0 
+    while x:
+        result ^= x & 1
+        x = x >> 1
+    return result 
 
 
 if __name__ == '__main__':
--- problem_mapping.js
+++ problem_mapping.js
@@ -24,7 +24,7 @@ problem_mapping = {
                 "total": 10000
             },
             "Python: parity.py": {
-                "passed": 0,
+                "passed": 10000,
                 "total": 10000
             }
         },
@@ -65,7 +65,7 @@ problem_mapping = {
                 "passed": 0,
                 "total": 10000
             },
-            "Python: closest_int_same_weight.py": { 
+            "Python: closest_int_same_weight.py": {
                 "passed": 0,
                 "total": 10000
             }
