edb231b47190b2a71b601d80068d07dbde96ba3c
dragfire <asem.devajit@gmail.com>
2020-09-16 23:52:39 -0600

    Update gitignore

--- epi_judge_java/rust/target/debug/deps/rust-22a0b655745fdf14.d
+++
@@ -1,5 +0,0 @@
-/home/dragfire/EPIJudge/epi_judge_java/rust/target/debug/deps/rust-22a0b655745fdf14.rmeta: src/lib.rs
-
-/home/dragfire/EPIJudge/epi_judge_java/rust/target/debug/deps/rust-22a0b655745fdf14.d: src/lib.rs
-
-src/lib.rs:
--- epi_judge_java/rust/target/debug/librust_ffi.d
+++
@@ -1,1 +0,0 @@
-/home/dragfire/EPIJudge/epi_judge_java/rust/target/debug/librust_ffi.so: /home/dragfire/EPIJudge/epi_judge_java/rust/src/lib.rs
