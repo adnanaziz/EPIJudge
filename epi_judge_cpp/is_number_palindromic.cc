e5075e9be82b07f2365dd9cfbef46cd29fe37978
Sanjay <panda.sanjay18@gmail.com>
2021-02-06 18:31:08 +0000

    Chapter 5.10

--- epi_judge_cpp/is_number_palindromic.cc
+++ epi_judge_cpp/is_number_palindromic.cc
@@ -1,6 +1,24 @@
 #include "test_framework/generic_test.h"
 bool IsPalindromeNumber(int x) {
   // TODO - you fill in here.
+  //0 - Check if number is negative
+  if(x <= 0) {
+    return x == 0;
+  }
+  //1. Count the number if digit
+  int num_of_digit = static_cast<int>(floor(log10(x))) + 1;
+  //2. Create a mask
+  int mask = static_cast<int>(pow(10, num_of_digit - 1));
+  //3. Check as palindram by checking first and last digit
+  for(int i = 0; i < num_of_digit / 2; i++){
+    if(x/mask != x%10){
+      return false;
+    }
+    x %= mask;
+    x /= 10;
+    mask/=100;
+
+  }
   return true;
 }
 
