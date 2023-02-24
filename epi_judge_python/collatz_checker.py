5bcc2a432a6847bca3e709ef0f30b4328e8a3743
Killian Cooney <59583502+invicta117@users.noreply.github.com>
2023-02-24 17:19:47 +0000

    Updating Collatz checker function
    removing the "test_" in the function definition

--- epi_judge_python/collatz_checker.py
+++ epi_judge_python/collatz_checker.py
@@ -1,7 +1,7 @@
 from test_framework import generic_test
 
 
-def test_collatz_conjecture(n: int) -> bool:
+def collatz_conjecture(n: int) -> bool:
     # TODO - you fill in here.
     return False
 
@@ -10,4 +10,4 @@ if __name__ == '__main__':
     exit(
         generic_test.generic_test_main('collatz_checker.py',
                                        'collatz_checker.tsv',
-                                       test_collatz_conjecture))
+                                       collatz_conjecture))
