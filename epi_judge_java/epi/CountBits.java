ea1e9d069e571b570d2af4eacff026f2b4ab6f3c
Simbarashe Dzinamarira <sdzinamarira@linkedin.com>
2024-01-24 14:31:59 -0800

    Count bits.

--- epi_judge_java/epi/CountBits.java
+++ epi_judge_java/epi/CountBits.java
@@ -5,8 +5,12 @@ public class CountBits {
   @EpiTest(testDataFile = "count_bits.tsv")
 
   public static short countBits(int x) {
-    // TODO - you fill in here.
-    return 0;
+    short numBits = 0;
+    while (x != 0) {
+      numBits += (x & 1);
+      x >>>= 1;
+    }
+    return numBits;
   }
 
   public static void main(String[] args) {
--- problem_mapping.js
+++ problem_mapping.js
@@ -6,7 +6,7 @@ problem_mapping = {
                 "total": 10001
             },
             "Java: CountBits.java": {
-                "passed": 0,
+                "passed": 10001,
                 "total": 10001
             },
             "Python: count_bits.py": {
@@ -65,7 +65,7 @@ problem_mapping = {
                 "passed": 0,
                 "total": 10000
             },
-            "Python: closest_int_same_weight.py": { 
+            "Python: closest_int_same_weight.py": {
                 "passed": 0,
                 "total": 10000
             }
