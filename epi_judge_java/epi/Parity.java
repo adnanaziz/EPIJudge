5ac210c75d7431c268430145ebd9397ef99ceea3
Simbarashe Dzinamarira <simbadzina@gmail.com>
2024-01-25 14:21:36 -0800

    Compute parity of a word. (#2)

--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -4,8 +4,12 @@ import epi.test_framework.GenericTest;
 public class Parity {
   @EpiTest(testDataFile = "parity.tsv")
   public static short parity(long x) {
-    // TODO - you fill in here.
-    return 0;
+    short parity = 0;
+    while (x != 0) {
+      parity ^= (x & 1);
+      x >>>= 1;
+    }
+    return parity;
   }
 
   public static void main(String[] args) {
